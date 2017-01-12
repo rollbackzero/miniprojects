12cb6916aea231abef293c9646588a9ec9b6c996
richardt <richard.tumaliuan@reannz.co.nz>
2017-01-12 14:20:28 +1300

    commiting

--- miniprojects.sh
+++ miniprojects.sh
@@ -2,6 +2,6 @@ git init
 git add .
 git commit -m "commiting"
 #git remote add origin git@github.com:rollbackzero/miniprojects.git
-git remove add origin https://github.com/rollbackzero/miniprojects.git
+git remote add origin https://github.com/rollbackzero/miniprojects.git
 git remote -v
 git push -u origin master
